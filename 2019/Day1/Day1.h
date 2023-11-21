20f718d9b79af85c8681a92093cb11984bbf9089
Brett Constantinoff <constantinoffbrett@gmail.com>
2023-11-20 17:10:47 -0700

    2019 day 1

---
+++ 2019/Day1/Day1.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "../../Common.h"
+
+template<class Output>
+class Day1 : public DayBase<Output>
+{
+public:
+    Day1(std::string file) : DayBase(file)
+    {
+        auto& utils = AoCUtilities::getInstance();
+        setOutput();
+
+        utils.display([this]() { this->part1(); }, false);
+        utils.display([this]() { this->part2(); }, true);
+    }
+    ~Day1() {}
+
+    void part1() override
+    {
+        auto& utils = AoCUtilities::getInstance();
+        int32_t sum{};
+        for (const auto& entry : m_output)
+        {
+            int32_t mass{ (utils.strToInt(entry) / 3) - 2};
+            sum += mass;
+        }
+        std::cout << sum;
+    }
+
+    void part2() override
+    {
+        auto& utils = AoCUtilities::getInstance();
+        int32_t sum{};
+        for (const auto& entry : m_output)
+            sum += recurse(utils.strToInt(entry));
+        std::cout << sum;
+    }
+
+    void setOutput() override
+    {
+        auto& utils = AoCUtilities::getInstance();
+        m_output = utils.fileToVector(m_file, '\n');
+    }
+
+private:
+    int32_t recurse(int32_t entry)
+    {
+        const int32_t fuel{ (entry / 3) - 2 };
+
+        if (fuel <= 0) return 0;
+
+        int32_t additionalFuel{ std::max(0, recurse(fuel)) };
+        return fuel + additionalFuel;
+    }
+};
---
+++ 2019/premake5.lua
@@ -0,0 +1,15 @@
+project "2019"
+    configurations { "Debug", "Release" }
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files { "**.cpp", "**.h" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
